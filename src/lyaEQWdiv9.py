3998c440cd6c0a8e697ac3ae75089b74a2c754e7
Maria Peña-Guerrero <pena@stsci.edu>
2013-04-22 22:23:37 -0400

    the tables import was not working because there were too many imports so I fixed that and added more IMFs

--- src/lyaEQWdiv9.py
+++ src/lyaEQWdiv9.py
@@ -4,9 +4,9 @@ import numpy
 import table
 import matplotlib
 matplotlib.use("macosx")
-from pprint import pprint
-from pylab import *
-from random import randrange
+#from pprint import pprint
+#from pylab import *
+#from random import randrange
 from matplotlib.ticker import MaxNLocator
 from spectrum import spectrum 
 from matplotlib import pyplot
@@ -42,20 +42,23 @@ fixed_ste = []
 fixed_neb = []
 fixed_tot = []
 i = 0
-IMFs = [#'0.5',
-        '1.2',
+IMFs = ['0.5',
+        '0.8',
+        '1.1',
+        #'1.2',
         #'1.3',
         #'1.4',
         #'1.5',
-        '1.6',
+        #'1.6',
         #'1.7',
         #'1.8',
         #'1.9', 
-        '2.0', 
+        #'2.0', 
         #'2.1',
         #'2.2',
-        '2.3']#,
-        #'2.5'] #
+        #'2.3',
+        #'2.5',
+        '2.6']
 
 total_chosenfiles = []
 
